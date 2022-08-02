d7f3ee62ca6ed57417a2868e3e4834ee2fb55174
“malltdev” <malltapps0@gmail.com>
2022-08-02 18:24:26 -0300

    finalizing tests

--- src/__tests__/Button.test.tsx
+++ src/__tests__/Button.test.tsx
@@ -1,5 +1,4 @@
 import { render, screen } from "@testing-library/react";
-// import userEvent from "@testing-library/user-event";
 import Button from "../components/Button";
 
 test("should return a button", async () => {
